86ba65ae8d41d706d0fb7c310db60264fcd8b979
alin090402 <alin090402@yahoo.ro>
2022-04-02 16:42:47 +0300

    Etapele 3-4

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-//const fs = require("fs");
-//const sharp  = require("sharp");
+const fs = require("fs");
+const sharp  = require("sharp");
+const {Client} = require("pg");
 
 app = express();
+
+//client = new Client({user:})
 
 app.set("view engine", "ejs");
 
@@ -10,11 +13,34 @@ app.use("/Resurse", express.static(__dirname + "/Resurse"));
 
 app.get(["/","/home","/index"], function(req, res){
     console.log(__dirname + "/index.html");
-    res.render("Pagini/index", {ip:req.ip});
+    res.render("Pagini/index", {ip:req.ip, imagini:SelectedImages});
     //res.sendFile(__dirname + "/index.html");
     
     res.end();
 
+})
+
+app.get("*/galerie-animata.css", function(req, res)
+{
+    var sirScss=fs.readFileSync(__dirname+"/resurse/resurse/scss/galerie_animata.scss").toString("utf8");
+    var culori = ["navy", "black", "purple", "grey"];
+    var indiceAleator = Math.floor(Math.random()*culori.length);
+    var culoareAleatoare = culori[indiceAleator];
+    rezScss = ejs.render(sirScss, {culoare: culoareAleatoare});
+    console.log(rezScss);
+    var caleScss=__dirname+"/temp/galerie_animata.scss";
+    fs.writeFileSync(__dirname+"/temp/galerie_animata.scss", rezScss);
+    try{
+    rezCompilare = sass.compile(caleScss, {sourceMap:true});
+    var caleCss=__dirname+"/temp/galerie_animata.css";
+    fs.writeFileSync(caleCss, rezCompilare.css);
+    res.setHeader("Content-Type", "text/css");
+    res.sendFile(caleCss);
+    }
+    catch(err){
+        console.log(err);
+        res.send("Eroare");
+    }
 })
 
 app.get("/*.ejs",function(req, res){
@@ -45,30 +71,70 @@ app.get("/*", function(req, res){
     res.end();
 })
 
-/*
 function creeazaImagini(){
-    console.log("creare imagini");
-    var buf=fs.readFileSync(__dirname+"/resurse/json/galerie.json").toString("utf8");
+    var buf=fs.readFileSync(__dirname+"/Resurse/Json/galerie.json").toString("utf8");
+
+
     obImagini=JSON.parse(buf);//global
+    SelectedImages = []
+
+
     //console.log(obImagini);
     for (let imag of obImagini.imagini){
+        if(SelectedImages.length >= 10)
+            break;
+        let today = new Date();
+        let sfert_ora = Math.floor(today.getMinutes() / 15) + 1;
+        console.log(sfert_ora);
+        if(imag.sfert_ora != sfert_ora)
+            continue;
+
         let nume_imag, extensie;
-        [nume_imag, extensie ]=imag.fisier.split(".")// "abc.de".split(".") ---> ["abc","de"]
+        [nume_imag, extensie ]=imag.fisier.split(".")// "abc.de".split(".") ---> ["abc","de"] imagine.png->imagine.webp
         let dim_mic=150
         
         imag.mic=`${obImagini.cale_galerie}/mic/${nume_imag}-${dim_mic}.webp` //nume-150.webp // "a10" b=10 "a"+b `a${b}`
         //console.log(imag.mic);
+        console.log(imag.titlu);
+
         imag.mare=`${obImagini.cale_galerie}/${imag.fisier}`;
         if (!fs.existsSync(imag.mic))
             sharp(__dirname+"/"+imag.mare).resize(dim_mic).toFile(__dirname+"/"+imag.mic);
+
+
+        let dim_mediu=300;
+        imag.mediu=`${obImagini.cale_galerie}/mediu/${nume_imag}-${dim_mediu}.png` 
+        //console.log(imag.mediu);
+        if (!fs.existsSync(imag.mediu))
+            sharp(__dirname+"/"+imag.mare).resize(dim_mediu).toFile(__dirname+"/"+imag.mediu);
+        SelectedImages.push(imag)
 
         
     }
 
 }
-creeazaImagini();
-*/
+function creeazaErori(){
+    var buf=fs.readFileSync(__dirname+"/Resurse/resurse/json/erori.json").toString("utf8");
+    obErori=JSON.parse(buf);//global
+    console.log(obErori);
+}
 
+function RandeazaEroare(identificator, status,titlu, text, imagine){
+
+    var eroare = obErori.erori.find( function(elem) {return elem.identificator == identificator})
+    if(status){
+        res.status(identificator).render("pagini/eroare_generala", {titlu:eroare.titlu, text:eroare.text, imagine:obErori.caleBaza + "/" + eroare.imagine})
+    }
+}
+
+
+
+
+creeazaImagini();
+creeazaErori();
+
+//console.log(SelectedImages);
+//console.log(obImagini);
 console.log("O pornit");
 
 app.listen(8080)
