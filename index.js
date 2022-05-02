dc6dcc39f69fe80142650aca2fa8e0019dbaee41
alin090402 <alin090402@yahoo.ro>
2022-05-02 10:21:54 +0300

    comentariu creeaza erori

--- index.js
+++ index.js
@@ -303,8 +303,8 @@ function creeazaImagini(){
 
 }
 function creeazaErori(){
-    var buf=fs.readFileSync(__dirname+"/Resurse/json/erori.json").toString("utf8");
-    obErori=JSON.parse(buf);//global
+    //var buf=fs.readFileSync(__dirname+"/Resurse/json/erori.json").toString("utf8");
+    //obErori=JSON.parse(buf);//global
     
 }
 
